d619379c43b521f05bdf4a4d5050720f5d7d2844
deepak7580 <mr.deepak7580@gmail.com>
2021-07-01 09:09:47 +0530

    todoApp

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-const App = () => {
-  let fruitList = [
-    "Apple",
-    "Mango",
-    "DragonFruit",
-    "PassionFruit",
-    "tomato",
-    "lichi",
-  ];
-  let [counter, setCounter] = useState(10);
+function App() {
+  let [list, setList] = useState([]);
 
-  const increment = () => {
-    // counter += 1;
-    setCounter(counter + 1);
-    console.log("I AM INCREMENT FUNCTION", counter);
+  let [todoText, setTodoText] = useState("");
+  let updateTodoText = (e) => {
+    setTodoText(e.target.value);
   };
+
+  let addToDo = () => {
+    const newList = [todoText, ...list];
+    setList(newList);
+
+    setTodoText("");
+  };
+
   return (
     <div>
-      <h1>
-        <u>List of fruits</u>:-
-      </h1>
-      {fruitList.map((item) => (
-        <ul>
-          <li>
-            <h3>{item}</h3>
-          </li>
-        </ul>
+      <h2 className="bg-dark text-light p-3" style={{ textAlign: "center" }}>
+        📝 ToDoList 📝
+      </h2>
+
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="text"
+            value={todoText}
+            onChange={updateTodoText}
+            className="form-control form-control-lg"
+            placeholder="Enter your task"
+          />
+        </div>
+
+        <div className="col-3">
+          <input
+            type="button"
+            className="btn btn-lg btn-secondary"
+            value="Add Todo"
+            onClick={addToDo}
+          />
+        </div>
+      </div>
+
+      {list.map((item, index) => (
+        <div key={index} className="alert alert-secondary my-2">
+          {item}
+        </div>
       ))}
-      <input type="button" value="INCREMENT" onClick={increment} />
-      <input type="text" value={counter}></input>
-      <div>😄</div>
+      {list.length == 0 && (
+        <div className="alert alert-dark my-1">You have no Pending Task</div>
+      )}
     </div>
   );
-};
+}
 
 export default App;
